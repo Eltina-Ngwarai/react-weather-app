e49024ec9a543f525f9227b9fad590c94d15200d
elty-annie <eltinangwarai@gmail.com>
2022-12-16 16:30:12 +0100

    added the weather app initial html

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-     Weather app
-     </div>
+        Weather app
+        <Weather />
+      </div>
     </div>
   );
 }
