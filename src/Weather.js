ec454b7c444ce7122f9febac97dfe2104510fa93
elty-annie <eltinangwarai@gmail.com>
2022-12-17 23:17:56 +0100

    added the app css

--- src/Weather.js
+++ src/Weather.js
@@ -1,38 +1,50 @@
 import React from "react";
-import "./Weather.css"
+import "./Weather.css";
 
-export default function Weather(){
-    return (
+export default function Weather() {
+  return (
     <div className="Weather">
-        <form>
-            <div className="row">
-                <div className="col-9">
-                    <input type="search" placeholder="Enter a city.." className="form-control" />
-                </div>
-                <div className="col-3">
-                    <input type="submit" value="Search" className="btn btn-primary" />
-                </div>
-            </div>
-        </form>
-        <h1>Hamburg</h1>
-        <ul>
-            <li>Wednesday</li>
-            <li>Partly Cloudy</li>  
-        </ul>
+      <form>
         <div className="row">
-            <div className="col-6">
-                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Partly Cloudy" />
-                6°C
-            </div>
-            <div className="col-6">
-                <ul>
-                    <li>Precipitation: 15%</li>
-                    <li>Humidity: 72%</li>
-                    <li>Wind: 2km/hr</li>
-                </ul>
-            </div>
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
         </div>
+      </form>
+      <h1>Hamburg</h1>
+      <ul>
+        <li>Wednesday</li>
+        <li>Partly Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Partly Cloudy"
+              className="float-left"
+            />
+            <div className="float-left temperatureContainer">
+              <span className="temperature">6</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 2km/hr</li>
+          </ul>
+        </div>
+      </div>
     </div>
-   
-    );
+  );
 }
