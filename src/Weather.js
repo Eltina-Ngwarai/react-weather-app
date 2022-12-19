d7d299a8852ec9193ce7f9272ae9568fd9548d42
elty-annie <eltinangwarai@gmail.com>
2022-12-19 21:58:30 +0100

    added date on weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,17 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
+import FormattedDate from "./FormattedDate";
 
 export default function Weather() {
-  const [weatherData, setWeatherData]=useState({ready:false});
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-  function handleResponse(response){
-console.log(response);
-
+  function handleResponse(response) {
     setWeatherData({
       ready: true,
       temperature: response.data.temperature.current,
       humidity: response.data.temperature.humidity,
-      date: "Monday 13:00",
+      date: new Date(response.data.time * 1000),
       description: response.data.condition.description,
       wind: response.data.wind.speed,
       city: response.data.city,
@@ -20,58 +19,64 @@ console.log(response);
     });
   }
   if (weatherData.ready) {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city.."
-              className="form-control"
-            />
-          </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary w-100"  />
-          </div>
-        </div>
-      </form>
-      
-      <h1>{weatherData.city}</h1>
-      <ul>
-        <li>{weatherData.date}</li>
-        <li className="text-capitalize">{weatherData.description}</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              src={weatherData.iconUrl}
-              alt={weatherData.description}
-              className="float-left"
-            />
-            <div className="float-left temperatureContainer">
-              <span className="temperature">{Math.round(weatherData.temperature)}</span>
-              <span className="unit">°C</span>
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            
-            <li>Humidity: {weatherData.humidity}</li>
-            <li>Wind: {Math.round (weatherData.wind)} km/hr</li>
-          </ul>
+        </form>
+
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                src={weatherData.iconUrl}
+                alt={weatherData.description}
+                className="float-left"
+              />
+              <div className="float-left temperatureContainer">
+                <span className="temperature">
+                  {Math.round(weatherData.temperature)}
+                </span>
+                <span className="unit">°C</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}</li>
+              <li>Wind: {Math.round(weatherData.wind)} km/hr</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "45d862b440fa3dtc36ab8b0be0f20o60";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Hamburg&key=${apiKey}&units=metric`;
 
-} else {
-  const apiKey ="45d862b440fa3dtc36ab8b0be0f20o60";
-  let apiUrl= `https://api.shecodes.io/weather/v1/current?query=Hamburg&key=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(handleResponse);
-  return "Loading..."
-}
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
