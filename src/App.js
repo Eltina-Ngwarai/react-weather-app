64df0bb01e7c6451daee4fd3f60d538435b91a59
elty-annie <eltinangwarai@gmail.com>
2022-12-26 00:36:52 +0100

    cleaned up the code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from "react"
-import './App.css';
+import React from "react";
+import "./App.css";
 import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-       
-        <Weather defaultCity="Harare"/>
+        <Weather defaultCity="Harare" />
         <footer>
-      <a
-        href="https://github.com/elty-annie/vanilla-weather-app"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Open-source code{" "}
-      </a>{" "}
-      by Eltina Ngwarai
-      </footer>
+          <a
+            href="https://github.com/elty-annie/vanilla-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code{" "}
+          </a>{" "}
+          by Eltina Ngwarai
+        </footer>
       </div>
     </div>
   );
-};
+}
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,38 +1,44 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-export default function WeatherTemperature(props){
-    const [unit, setUnit]=useState("celcius");
-    function showFahrenheit(event){
-        event.preventDefault();
-        setUnit("fahrenheit");
-    }
-    function showCelcius (event){
-        event.preventDefault();
-        setUnit("celcius");
-    }
-    function fahrenheit (){
-        return (props.celcius * 9) / 5 + 32;
-    }
+export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("celcius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function showCelcius(event) {
+    event.preventDefault();
+    setUnit("celcius");
+  }
+  function fahrenheit() {
+    return (props.celcius * 9) / 5 + 32;
+  }
 
-    if (unit === "celcius"){
-
-    return(
-        <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celcius)}
+  if (unit === "celcius") {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.celcius)}</span>
+        <span className="unit">
+          °C |{" "}
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>
         </span>
-        <span className="unit">°C |{" "}
-         <a href="/" onClick={showFahrenheit}>°F</a></span>
-        </div>
+      </div>
     );
-    } else {
-        return (
-            <div className="WeatherTemperature">
-        <span className="temperature">
-            {Math.round(fahrenheit())}
+  } else {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(fahrenheit())}</span>
+        <span className="unit">
+          {" "}
+          <a href="/" onClick={showCelcius}>
+            {" "}
+            °C{" "}
+          </a>{" "}
+          |°F
         </span>
-        <span className="unit"> <a href="/" onClick={showCelcius}> °C </a>{" "} |°F</span>
-        </div>
+      </div>
     );
-        
-    }
+  }
 }
