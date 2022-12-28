d4473c671e8195dd4ce7650744608c04a2683931
elty-annie <eltinangwarai@gmail.com>
2022-12-28 16:17:40 +0100

    added forecast for 1 day

--- src/Weatherforecast.js
+++ src/Weatherforecast.js
@@ -1,32 +1,34 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
 import "./Weatherforecast.css";
 import axios from "axios";
-
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props){
-    function handleResponse(response){
+    let [loaded, setLoaded] = useState (false);
+    let  [forecast, setForecast] = useState (null);
 
+    function handleResponse(response){
+        setForecast(response.data.daily);
+        setLoaded (true);
     }
-    let apiKey = "c5f0e59acac64258bb92ed027d20c68f";
+    if(loaded){
+        console.log(forecast)
+        return (
+            <div className="WeatherForecast">
+                <div className="row">
+                     <div className="col">
+                   <WeatherForecastDay data={forecast[0]}/>
+                    </div>
+                </div>    
+            </div>
+        );
+    } else {
+    let apiKey = "40b745c14eadad7b7c4e6e4bf3b70103";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
     axios.get(apiUrl).then(handleResponse)
-    return (
-        <div className="WeatherForecast">
-            <div className="col">
-                <div className="WeatherForecastDay">
-                    Thu
-                </div>
-                <WeatherIcon code="01d" size={36}/>
-                <div className="WeatherForecast-temperature">
-                    <span className="WeatherForecast-temperature-max">19°C</span>
 
-                    <span className="WeatherForecast-temperature-min">10°C</span>
-                </div>
-            </div>
-        </div>
-    )
-}
+    return null;
+    }
+    }
